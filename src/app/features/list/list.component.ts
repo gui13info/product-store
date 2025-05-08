91ec79ec0a78bd3f90605350934f9af40a189a9b
Guilherme Marques <marques.guilherme0067@gmail.com>
2025-05-08 11:19:16 -0300

    FEAT: Adiciona card estilizado para exibição do produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,12 +1,15 @@
 import { Component, inject } from '@angular/core';
 
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
 import { ProductI } from '../../shared/interfaces/product.interface';
 import { ProductsService } from '../../shared/services/products.service';
 
 @Component({
     selector: 'app-list',
     standalone: true,
-    imports: [],
+    imports: [MatCardModule, MatButtonModule],
     templateUrl: './list.component.html',
     styleUrl: './list.component.scss',
 })
